adc10becf6eac13b9c75c7b11b8e18dfb46d14d3
Ari Chivukula <ari@chivuku.la>
2019-07-06 17:32:00 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
 import 'package:flutter_web/material.dart';
 
-void main() {
+Future<void> readStatute() async {
   var statute = await http.readBytes("/assets/statute.csv");
   print(statute);
+}
+
+void main() {
+  readStatute();
   runApp(new Text('Hello World', textDirection: TextDirection.ltr));
 }
