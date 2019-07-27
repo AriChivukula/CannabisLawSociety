14eef35ff3e1d7d74a48f3726654e519c4013575
Ari Chivukula <ari@chivuku.la>
2019-07-27 12:15:19 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter_web/material.dart';
+import 'package:csv/csv.dart' as csv;
 import 'package:http/http.dart' as http;
 
-Future<void> readStatute() async {
+Future<List<List<string>>> readStatute() async {
   var statute = await http.get("/assets/statute.csv");
-  print(statute);
+  print(statute.body);
+  return csv.CsvToListConverter(shouldParseNumbers: false).convert(statute.body);
+}
+
+Future<void> renderStatute() async {
+  var statuteList = await readStatute();
+  print(statuteList);
+  runApp(new Text('Hello World', textDirection: TextDirection.ltr));
 }
 
 void main() {
-  readStatute();
-  runApp(new Text('Hello World', textDirection: TextDirection.ltr));
+  render();
 }
