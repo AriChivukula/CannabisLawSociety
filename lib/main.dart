309de2f8c17f54e4ad8ac074e4ae424efb80b8d8
Ari Chivukula <ari@chivuku.la>
2019-07-27 12:34:51 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,31 @@ import 'package:http/http.dart' as http;
 
 Future<List<List<string>>> readStatute() async {
   var statute = await http.get("/assets/statute.csv");
-  print(statute.body);
+  print(statute.body); // TODO: remove
   return csv.CsvToListConverter(shouldParseNumbers: false).convert(statute.body);
 }
 
 Future<void> renderStatute() async {
   var statuteList = await readStatute();
-  print(statuteList);
-  runApp(new Text('Hello World', textDirection: TextDirection.ltr));
+  print(statuteList); // TODO: remove
+  runApp(Table(
+    border: TableBorder.all(width: 1.0, color: Colors.black),
+    children: [
+      for (var statuteListRow in statuteList)
+        TableRow(
+          children: [
+            for (var statuteListCell in statuteListRow)
+              TableCell(
+                child: Row(
+                  children: new Text(statuteListCell),
+                ),
+              ),
+          ],
+        ),
+    ],
+  ));
 }
 
 void main() {
-  render();
+  renderStatute();
 }
