acee779ccd527852525198c9abe7fd4f7efc5026
Ari Chivukula <ari@chivuku.la>
2019-07-27 12:56:41 -0700

    Fix UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,13 @@ import 'package:flutter_web/material.dart';
 import 'package:csv/csv.dart' as csv;
 import 'package:http/http.dart' as http;
 
-Future<List<List<String>>> readStatute() async {
+Future<List<List<dynamic>>> readStatute() async {
   var statute = await http.get("/assets/statute.csv");
-  print(statute.body); // TODO: remove
   return csv.CsvToListConverter(shouldParseNumbers: false).convert(statute.body);
 }
 
 Future<void> renderStatute() async {
   var statuteList = await readStatute();
-  print(statuteList); // TODO: remove
   runApp(Padding(
     padding: const EdgeInsets.all(5.0),
     child: Table(
@@ -24,7 +22,7 @@ Future<void> renderStatute() async {
                   verticalAlignment: TableCellVerticalAlignment.middle,
                   child: Padding(
                     padding: EdgeInsets.all(5.0),
-                    child: new Text(statuteListCell),
+                    child: new Text(statuteListCell as String),
                   ),
                 ),
             ],
